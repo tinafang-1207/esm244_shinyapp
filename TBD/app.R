a49352a16f5c20e2ebe5234a751115bd09a5014b
tinafang-1207 <yutianfang@ucsb.edu>
2022-02-03 12:37:32 -0800

    add some graph

--- TBD/app.R
+++ TBD/app.R
@@ -3,11 +3,11 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(janitor)
+library(lubridate)
 
 fish <- read_csv(here("TBD", "data", "LTER_reef_fish.csv"))
 fish_clean <- fish %>% 
   clean_names()
-
 
 
 ### 2. create user interface:
@@ -19,16 +19,11 @@ ui <- fluidPage(
                               label = "Choose treatment:",
                               choices = c("Control" = "CONTROL",
                                           "Annual" = "ANNUAL",
-                                          "Continual" = "CONTINUAL")),
-                 selectInput(inputId = "pt_color",
-                             label = "Choose point color:",
-                             choices = c("Awesome red!" = "red",
-                                         "Beautiful blue!" = "blue",
-                                         "Ragin' Orange!" = "orange"))
+                                          "Continual" = "CONTINUAL"))
     ), # end sidebar panel
     
     mainPanel("put my graph in here",
-              plotOutput(outputId = "penguin_plot"))
+              plotOutput(outputId = "treatment_plot"))
   ) # end sidebarLayout
 )
 
@@ -36,14 +31,17 @@ ui <- fluidPage(
 server <- function(input, output) {
   treatment_select <- reactive({
     fish_clean %>%
-      select(year,count,treatment) %>% 
-      group_by(year) %>% 
+      mutate(year = as.factor(year)) %>%
+      mutate(count = as.factor(count)) %>%
+      group_by(year,treatment) %>%
+      summarize(count=n()) %>%
+      filter(year %in% c("2010":"2020")) %>%
       filter(treatment == input$kelp_treatment)
   }) # end penguin_select reactive
   
-  output$penguin_plot <- renderPlot({
+  output$treatment_plot <- renderPlot({
     ggplot(data = treatment_select(), aes(x = year, y = count)) +
-      geom_line(color = input$pt_color)
+      geom_col()
   })
 }
 
